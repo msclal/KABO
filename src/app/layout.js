77c072ee1ab003485feaf7d4d7b3dd4697b29dd1
msclal <mika1shanela@gmail.com>
2023-09-14 12:03:50 -0700

    add custom conts and colors

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,13 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 
-const inter = Inter({ subsets: ["latin"] });
+const magilio = localFont({
+  src: "../../public/fonts/magilio.ttf",
+});
+
+const nevis = localFont({
+  src: "../../public/fonts/nevis.ttf",
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +17,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${magilio.className} ${nevis.className}`}>
+        {children}
+      </body>
     </html>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,14 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        kabo: {
+          gold: "#FEC430",
+          gray: "#242424",
+          black: "#1B1B1B",
+          offWhite: "#EDEAE1",
+        },
+      },
     },
   },
   plugins: [],
