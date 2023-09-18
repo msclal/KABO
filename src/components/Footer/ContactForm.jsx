cba8453f064a12e0ef793b7ba041779b2440ff33
msclal <mika1shanela@gmail.com>
2023-09-18 13:27:52 -0700

    email prob works (prob)

--- src/components/Footer/ContactForm.jsx
+++ src/components/Footer/ContactForm.jsx
@@ -1,45 +1,35 @@
 "use client";
-import { useState } from "react";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
 import { useToast } from "@/components/ui/use-toast";
-// import axios from "axios";
 export default function ContactForm() {
-  const [form, setForm] = useState({ name: "", email: "", message: "" });
   const { toast } = useToast();
 
-  const handleChange = (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
-    setForm((prevState) => ({ ...prevState, [name]: value }));
-  };
-
-  const handleSend = async () => {
-    if (form.name !== "" && form.email !== "" && form.message !== "") {
-      console.log("Send");
-
-      setForm({ name: "", email: "", message: "" });
-      toast({
-        variant: "success",
-        title: "Message Sent!",
-        description: "We'll get back to you as soon as possible.",
-      });
-
-      // await axios
-      //   .post('/api/email/', form)
-      //   .then((res) => {
-      //     console.log(res);
-      //   })
-      //   .catch((err) => {
-      //     console.log(err);
-      //   })
-      //   .finally(() => {
-      //     setForm({ name: "", email: "", message: "" });
-      //     toast({
-      //       variant: "success",
-      //       title: "Got your Message!",
-      //       description: "We'll get back to you as soon as possible.",
-      //     });
-
-      //   });
+  const form = useRef();
+  const handleSend = (e) => {
+    e.preventDefault();
+    if (e.target[0].value && e.target[1].value && e.target[2].value) {
+      emailjs
+        .sendForm(
+          "service_hy4bsvp",
+          "template_9e567ar",
+          form.current,
+          "B0GWTfpimt3X7kH-y",
+        )
+        .then(
+          (result) => {},
+          (error) => {
+            console.log(error.text);
+          },
+        )
+        .finally(() => {
+          e.target.reset();
+          toast({
+            variant: "success",
+            title: "Message Sent!",
+            description: "We'll get back to you as soon as possible.",
+          });
+        });
     } else {
       toast({
         variant: "destructive",
@@ -58,47 +48,44 @@ export default function ContactForm() {
           fans worldwide.
         </p>
       </div>
-      <div className="w-full lg:w-[40%] flex flex-col gap-y-5 justify-end">
+      <form
+        ref={form}
+        onSubmit={handleSend}
+        className="w-full lg:w-[40%] flex flex-col gap-y-5 justify-end"
+      >
         <div className="flex gap-x-10">
-          <div className="">
-            <p className="font-semibold">Name*</p>
+          <div>
+            <label className="font-semibold">Name*</label>
             <input
               type="text"
               name="name"
-              value={form.name}
-              onChange={handleChange}
               className="bg-kabo-white border-b border-kabo-gray w-full text-lg md:text-2xl text-kabo-black focus:outline-none"
             />
           </div>
           <div className="md:w-96 lg:w-full">
-            <p className="font-semibold">Email*</p>
+            <label className="font-semibold">Email*</label>
             <input
-              type="text"
+              type="email"
               name="email"
-              value={form.email}
-              onChange={handleChange}
               className="bg-kabo-white border-b border-kabo-gray w-full text-lg md:text-2xl text-kabo-black focus:outline-none"
             />
           </div>
         </div>
         <div>
           <div className="flex flex-col">
-            <p className="font-semibold pb-3">Message*</p>
+            <label className="font-semibold pb-3">Message*</label>
             <textarea
               name="message"
-              value={form.message}
-              onChange={handleChange}
               className="bg-kabo-white min-h-[100px] md:min-h-[120px] border-kabo-gray text-xl md:text-2xl text-kabo-black max-h-64 focus:outline-none border"
             />
           </div>
         </div>
-        <p
-          onClick={handleSend}
+        <input
+          type="submit"
+          value="Send"
           className="self-end border-[0.5px] border-kabo-gray sm:hover:bg-kabo-gray sm:hover:text-kabo-white cursor-pointer rounded-3xl w-fit px-10 h-fit py-[2px] duration-300"
-        >
-          Send
-        </p>
-      </div>
+        />
+      </form>
     </div>
   );
 }
