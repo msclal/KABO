03921c64bac117f985bc8949659ba1bf39f9654e
msclal <mika1shanela@gmail.com>
2023-09-17 18:19:39 -0700

    create mobile projects

---
+++ src/components/Projects/ProjectMobile.jsx
@@ -0,0 +1,94 @@
+"use client";
+import { useState } from "react";
+import Image from "next/image";
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import Uranus from "@/images/uranus.png";
+import Desktop from "@/images/projects_desktop.png";
+import Example from "@/images/projects_mobile.png";
+import MarqueeScroll from "./MarqueeScroll";
+
+export default function ProjectMobile({ id, project, isOpen, handleClick }) {
+  const { title, type, year, content, tools, imagesMobile } = project;
+  return (
+    <AccordionItem value={id}>
+      <AccordionTrigger
+        onClick={() => {
+          if (isOpen) {
+            handleClick("");
+          } else {
+            handleClick(id);
+          }
+        }}
+        // WHITE BG
+        className={`${id == "item-1" && `border-t border-t-kabo-white`} ${
+          isOpen && `bg-kabo-white text-kabo-black px-5 duration-300`
+        }`}
+
+        // BLACK BG
+        // className={`${id == "item-1" && `border-t border-t-kabo-white`} ${
+        //   isOpen && `bg-kabo-black duration-300`
+        // // }`}
+      >
+        <div className="flex justify-between w-full px-7">
+          <p>{title}</p>
+          <p className="max-sm:hidden">{type}</p>
+          <p>{year}</p>
+        </div>
+      </AccordionTrigger>
+      <AccordionContent>
+        <div className="w-full justify-items-center p-7 max-sm:space-y-5">
+          <p className="text-left font-semibold text-lg sm:hidden">{type} </p>
+          <div className="space-y-5 text-sm">
+            {content.map((paragraph, index) => (
+              <p key={index}>{paragraph}</p>
+            ))}
+            <div className="flex gap-x-5 pt-5 flex-wrap gap-y-3">
+              {tools.map((tool, index) => (
+                <span
+                  className="border-[0.5px] border-kabo-white rounded-3xl w-fit px-5 h-fit py-[2px]"
+                  key={index}
+                >
+                  {tool}
+                </span>
+              ))}
+              {/* <p className="border-[0.5px] border-kabo-white rounded-3xl w-fit px-5 h-fit py-[2px]"> */}
+              {/* Figma
+              </p>
+              <p className="border-[0.5px] border-kabo-white rounded-3xl w-fit px-5 h-fit py-[2px]">
+                Next.js
+              </p>
+              <p className="border-[0.5px] border-kabo-white rounded-3xl w-fit px-5 h-fit py-[2px]">
+                TailwindCSS
+              </p>
+              <p className="border-[0.5px] border-kabo-white rounded-3xl w-fit px-5 h-fit py-[2px]">
+                Vercel
+              </p>
+              <p className="border-[0.5px] border-kabo-white rounded-3xl w-fit px-5 h-fit py-[2px]">
+                Resend
+              </p> */}
+            </div>
+          </div>
+        </div>
+        <div className="flex gap-x-3 scrollbar-none w-full pb-4">
+          {imagesMobile.map((image, index) => (
+            <Image
+              key={index}
+              src={image}
+              alt={`Image ${index + 1}`}
+              className="w-full"
+            />
+          ))}
+          {/* <Image src={Example} alt="example" className="w-full" />
+          <Image src={Example} alt="example" className="w-full" />
+          <Image src={Example} alt="example" className="w-full" /> */}
+        </div>
+      </AccordionContent>
+    </AccordionItem>
+  );
+}
