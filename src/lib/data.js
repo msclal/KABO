083ce2b0ca8f2e5dcf3952d73817e5e421bfd883
msclal <mika1shanela@gmail.com>
2023-09-18 11:19:13 -0700

    move projects to data.js

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,3 +1,8 @@
+import Uranus from "@/images/uranus.png";
+import Desktop from "@/images/projects_desktop.png";
+import Example from "@/images/projects_mobile.png";
+
+// NAVBAR TABS
 export const sections = [
   { id: "home", label: "HOME" },
   { id: "info", label: "INFO" },
@@ -5,53 +10,54 @@ export const sections = [
   { id: "contact", label: "CONTACT" },
 ];
 
-// export const projectList = [
-//   {
-//     id: "item-1",
-//     title: "BELOW ZERO",
-//     type: "WEB DESIGN & DEVELOPMENT",
-//     year: "2023",
-//     logo: Uranus,
-//     link: "uranus27.com",
-//     content: [
-//       'Olivia Rodrigo, the rising star of the music industry, has captivated the hearts of millions with her soulful melodies and heartfelt lyrics. With her debut album "SOUR," Olivia has proven herself as a force to be reckoned with in the world of pop music.',
+// PROJECTS SECTION LIST
+export const projectList = [
+  {
+    id: "item-1",
+    title: "BELOW ZERO",
+    type: "WEB DESIGN & DEVELOPMENT",
+    year: "2023",
+    logo: Uranus,
+    link: "uranus27.com",
+    content: [
+      'Olivia Rodrigo, the rising star of the music industry, has captivated the hearts of millions with her soulful melodies and heartfelt lyrics. With her debut album "SOUR," Olivia has proven herself as a force to be reckoned with in the world of pop music.',
 
-//       'Her emotional performances on stage leave audiences spellbound, and her relatable lyrics resonate with listeners of all ages. From "drivers license" to "good 4 u," Olivias songs are anthems of generation, exploring the rollercoaster of emotions that come with love and heartbreak.',
-//     ],
-//     tools: ["Figma", "Next.js", "TailwindCSS", "Vercel"],
-//     imagesDesktop: [Desktop, Desktop, Desktop],
-//     imagesMobile: [Example, Example, Example],
-//   },
-//   {
-//     id: "item-2",
-//     title: "STIMULANT",
-//     type: "WEB DESIGN & DEVELOPMENT",
-//     year: "2023",
-//     logo: Uranus,
-//     link: "uranus27.com",
-//     content: [
-//       'Olivia Rodrigo, the rising star of the music industry, has captivated the hearts of millions with her soulful melodies and heartfelt lyrics. With her debut album "SOUR," Olivia has proven herself as a force to be reckoned with in the world of pop music.',
+      'Her emotional performances on stage leave audiences spellbound, and her relatable lyrics resonate with listeners of all ages. From "drivers license" to "good 4 u," Olivias songs are anthems of generation, exploring the rollercoaster of emotions that come with love and heartbreak.',
+    ],
+    tools: ["Figma", "Next.js", "TailwindCSS", "Vercel"],
+    imagesDesktop: [Desktop, Desktop, Desktop],
+    imagesMobile: [Example, Example, Example],
+  },
+  {
+    id: "item-2",
+    title: "STIMULANT",
+    type: "WEB DESIGN & DEVELOPMENT",
+    year: "2023",
+    logo: Uranus,
+    link: "uranus27.com",
+    content: [
+      'Olivia Rodrigo, the rising star of the music industry, has captivated the hearts of millions with her soulful melodies and heartfelt lyrics. With her debut album "SOUR," Olivia has proven herself as a force to be reckoned with in the world of pop music.',
 
-//       'Her emotional performances on stage leave audiences spellbound, and her relatable lyrics resonate with listeners of all ages. From "drivers license" to "good 4 u," Olivias songs are anthems of generation, exploring the rollercoaster of emotions that come with love and heartbreak.',
-//     ],
-//     tools: ["Figma", "Next.js", "TailwindCSS", "Vercel"],
-//     imagesDesktop: [Desktop, Desktop, Desktop],
-//     imagesMobile: [Example, Example, Example],
-//   },
-//   {
-//     id: "item-3",
-//     title: "URANUS 27",
-//     type: "WEB DESIGN",
-//     year: "2023",
-//     logo: Uranus,
-//     link: "uranus27.com",
-//     content: [
-//       'Olivia Rodrigo, the rising star of the music industry, has captivated the hearts of millions with her soulful melodies and heartfelt lyrics. With her debut album "SOUR," Olivia has proven herself as a force to be reckoned with in the world of pop music.',
+      'Her emotional performances on stage leave audiences spellbound, and her relatable lyrics resonate with listeners of all ages. From "drivers license" to "good 4 u," Olivias songs are anthems of generation, exploring the rollercoaster of emotions that come with love and heartbreak.',
+    ],
+    tools: ["Figma", "Next.js", "TailwindCSS", "Vercel"],
+    imagesDesktop: [Desktop, Desktop, Desktop],
+    imagesMobile: [Example, Example, Example],
+  },
+  {
+    id: "item-3",
+    title: "URANUS 27",
+    type: "WEB DESIGN",
+    year: "2023",
+    logo: Uranus,
+    link: "uranus27.com",
+    content: [
+      'Olivia Rodrigo, the rising star of the music industry, has captivated the hearts of millions with her soulful melodies and heartfelt lyrics. With her debut album "SOUR," Olivia has proven herself as a force to be reckoned with in the world of pop music.',
 
-//       'Her emotional performances on stage leave audiences spellbound, and her relatable lyrics resonate with listeners of all ages. From "drivers license" to "good 4 u," Olivias songs are anthems of generation, exploring the rollercoaster of emotions that come with love and heartbreak.',
-//     ],
-//     tools: ["Figma", "Next.js", "TailwindCSS", "Vercel"],
-//     imagesDesktop: [Desktop, Desktop, Desktop],
-//     imagesMobile: [Example, Example, Example],
-//   },
-// ];
+      'Her emotional performances on stage leave audiences spellbound, and her relatable lyrics resonate with listeners of all ages. From "drivers license" to "good 4 u," Olivias songs are anthems of generation, exploring the rollercoaster of emotions that come with love and heartbreak.',
+    ],
+    tools: ["Figma", "Next.js", "TailwindCSS", "Vercel"],
+    imagesDesktop: [Desktop, Desktop, Desktop],
+    imagesMobile: [Example, Example, Example],
+  },
+];
