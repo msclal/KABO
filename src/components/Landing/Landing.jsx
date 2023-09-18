484384f12196a615c40cb8e486d8f5cb57610a80
msclal <mika1shanela@gmail.com>
2023-09-17 18:16:58 -0700

    create landing

---
+++ src/components/Landing/Landing.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import Image from "next/image";
+import LandingCard from "./LandingCard";
+import LogoDesktop from "@/images/landing_desktop.svg";
+import LogoMobile from "@/images/landing_mobile.svg";
+
+export default function Hero() {
+  return (
+    <div className="flex flex-col px-5 sm:px-8 lg:px-16">
+      <div className="flex justify-between flex-wrap relative overflow-hidden">
+        <div className="max-sm:hidden w-[800px] lg:w-[1000px] pt-32">
+          <Image
+            src={LogoDesktop}
+            width={700}
+            height={500}
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw, 100vw"
+            className="object-contain w-full"
+            alt="KABO"
+            // priority={true}
+          />
+        </div>
+        <div className="sm:hidden w-[400px] pt-40">
+          <Image
+            src={LogoMobile}
+            width={700}
+            height={500}
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw, 100vw"
+            className="object-contain w-full"
+            alt="KABO"
+            // priority={true}
+          />
+        </div>
+        <div className="flex-grow">
+          <LandingCard />
+        </div>
+      </div>
+      <p className="absolute bottom-5 text-base font-outfit text-center text-kabo-white sm:text-left max-sm:px-0">
+        From the collaborative work of Bobby and Mika.
+      </p>
+    </div>
+  );
+}
+
+{
+  /* <div className="flex items-end text-kabo-white gap-x-5 bg-slate-500">
+        <p className="font-magilio text-9xl md:text-[13rem] xl:text-[20rem]">
+        KABO!
+        </p>
+        <p className="text-xl md:text-2xl xl:text-5xl mt-28 font-nevis">
+        [STUDIO]
+        </p>
+    </div> */
+}
---
+++ src/components/Landing/LandingCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function AboutCard() {
+  return (
+    <div className="border-r-[1px] border-r-kabo-gold pr-3 font-outfit font-light text-sm sm:text-base w-[350px] text-right h-fit ml-auto mt-12 sm:mt-5 lg:mt-0 lg:absolute lg:right-0 text-kabo-white max-[360px]:hidden">
+      We empower businesses to convey their vision through authentic and
+      meticulous design, ensuring lasting online digital impact and resonance
+      with consumers
+    </div>
+  );
+}
