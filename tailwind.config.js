5b2d3ca7d5adcd6e286a7eaa7cd317266c2b51c8
msclal <mika1shanela@gmail.com>
2023-09-17 18:24:21 -0700

    shadcn stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -13,7 +14,7 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       fontFamily: {
-        serif: ["var(--font-outfit)"],
+        outfit: ["var(--font-outfit)"],
         magilio: ["var(--font-magilio)"],
         nevis: ["var(--font-nevis)"],
       },
@@ -22,10 +23,31 @@ module.exports = {
           gold: "#FEC430",
           gray: "#242424",
           black: "#1B1B1B",
-          offWhite: "#EDEAE1",
+          white: "#EDEAE1",
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        // ["infinite-slider"]: "infiniteSlider 20s linear infinite",
+      },
+      keyframes: {
+        // infiniteSlider: {
+        //   "0%": { transform: "translateX(0)" },
+        //   "100%": {
+        //     transform: "translateX(calc(-250px * 15))",
+        //   },
+        // },
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
         },
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate"), require("tailwind-scrollbar")],
 };
